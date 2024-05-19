5ba6aeea4872182aa141b6f4c7eba8e7d261ffad
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-19 13:27:54 +0530

    todo version 0.1

---
+++ script.js
@@ -0,0 +1,7 @@
+const todoArray = [];
+
+function addTodo () {
+    const inputElement = document.querySelector('.js-todo-name');
+    const todoName = inputElement.value;
+    console.log(todoName); 
+}
