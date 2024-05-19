5a80c8496443abdd62f5df14417476d9ae9ef0a5
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-19 19:00:30 +0530

    added todo showing

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const todoArray = [];
 
+function renderTodo (){
+    let todoList = '';
+    
+    for(let i=0; i<todoArray.length;i++){
+        let html = `<p>${todoArray[i]}</p>`;
+        todoList +=html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoList;
+}
+
+
 function addTodo () {
     const inputElement = document.querySelector('.js-todo-name');
     const todoName = inputElement.value;
     console.log(todoName); 
     todoArray.push(todoName);
     console.log(todoArray);
+    inputElement.value='';
+    renderTodo();
 }
+
+function handleTodoKeydown(event) {
+    if (event.key === 'Enter') {
+      addTodo();
+    }
+  }
